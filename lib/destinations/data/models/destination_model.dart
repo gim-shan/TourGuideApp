d53adb492905b7ec117a050fed89a1a5331b2710
sineth-Hasintha <sinethhasintha@gmail.com>
2026-02-19 15:04:00 +0530

    Added file structure

---
+++ lib/destinations/data/models/destination_model.dart
@@ -0,0 +1,13 @@
+class Destination {
+  final String name;
+  final String location;
+  final String description;
+  final String imagePath;
+
+  Destination({
+    required this.name,
+    required this.location,
+    required this.description,
+    required this.imagePath,
+  });
+}
